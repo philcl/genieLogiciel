697ec0f34a40f0312f3afdf1145226d1546cc517
philcl <phil13420@live.fr>
2019-12-16 21:19:53 +0100

    Ajout de la connection vers MySQL avec hibernate

---
+++ Hibernate/src/DataBase/TicketEntity.java
@@ -0,0 +1,36 @@
+package DataBase;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Ticket", schema = "GenieLog", catalog = "")
+public class TicketEntity {
+    private int id;
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TicketEntity that = (TicketEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
