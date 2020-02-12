31b6035c5e4e0859c5b57736c4ed263731101a79
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-12 09:21:13 +0100

    Update RessourceClient.java

--- GenieLogiciel/src/API_REST/RessourceClient.java
+++ GenieLogiciel/src/API_REST/RessourceClient.java
@@ -2,7 +2,6 @@ package API_REST;
 
 import DataBase.AdresseEntity;
 import DataBase.ClientEntity;
-import Modele.Adresse;
 import Modele.Client.Client;
 import Modele.Client.ClientList;
 import Modele.Staff.Token;
@@ -13,13 +12,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.text.Normalizer;
+import javax.persistence.NoResultException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.NoResultException;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
 
 @Path("/client")
 public class RessourceClient {
@@ -34,27 +34,29 @@ public class RessourceClient {
         try {
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(jsonStr);
-            token = (String)json.get("token");
-            clientName = (String)json.get("clientName");
+            token = (String) json.get("token");
+            clientName = (String) json.get("clientName");
         } catch (ParseException | NullPointerException e) {
             e.printStackTrace();
             return ReponseType.getNOTOK("Il manque des parametres (token, clientName)", false, null, null);
         }
 
-        if(!Token.tryToken(token))
+        if (!Token.tryToken(token))
             return Token.tokenNonValide();
 
-        try(Session session = CreateSession.getSession()) {
+        try (Session session = CreateSession.getSession()) {
             tx = session.beginTransaction();
             clientId = (int) session.createQuery("SELECT c.siren FROM ClientEntity c WHERE c.nom = '" + clientName + "'").getSingleResult();
             tx.commit();
             session.clear();
             session.close();
         } catch (HibernateException e) {
-            if(tx != null)
+            if (tx != null)
                 tx.rollback();
             e.printStackTrace();
-        } catch (NoResultException e) {return ReponseType.getNOTOK("Le client " + clientName + " n'existe pas", false, null, null);}
+        } catch (NoResultException e) {
+            return ReponseType.getNOTOK("Le client " + clientName + " n'existe pas", false, null, null);
+        }
 
         return ReponseType.getOK(clientId);
     }
@@ -76,7 +78,7 @@ public class RessourceClient {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        if(!Token.tryToken(token))
+        if (!Token.tryToken(token))
             return Token.tokenNonValide();
 
         try (Session session = CreateSession.getSession()) {
@@ -84,33 +86,17 @@ public class RessourceClient {
             List clients = session.createQuery("FROM ClientEntity ").list();
             for (Object o : clients) {
                 ClientEntity client = (ClientEntity) o;
-                if(client.getActif() == 1) {
+                if (client.getActif() == 1) {
                     ClientList myClient = new ClientList();
                     myClient.name = client.getNom();
                     myClient.SIREN = client.getSiren();
 
-                    List temp = session.createQuery("SELECT t FROM AdresseEntity t WHERE t.idAdresse = " + client.getAdresse()).list();
+                    AdresseEntity adresseEntity = (AdresseEntity) session.createQuery("SELECT t FROM AdresseEntity t WHERE t.idAdresse = " + client.getAdresse()).getSingleResult();
 
-                    if(temp.isEmpty())
-                    {
-                        //throw new Exception("Pas d'adresse pour ce client");
-                        System.out.println("Pas d'adresse pour ce client");
-                    }
-                    else if(temp.size()!=1)
-                    {
-                        System.out.println("ID adresse dupliquée");
-                    }
-                    else
-                    {
-                        for (Object p : temp)
-                        {
-                            AdresseEntity adresseEntity = (AdresseEntity) p;
-                            myClient.adresse.numero = adresseEntity.getNumero();
-                            myClient.adresse.codePostal = adresseEntity.getCodePostal();
-                            myClient.adresse.rue = adresseEntity.getRue();
-                            myClient.adresse.ville = adresseEntity.getVille();
-                        }
-                    }
+                    myClient.adresse.numero = adresseEntity.getNumero();
+                    myClient.adresse.codePostal = adresseEntity.getCodePostal();
+                    myClient.adresse.rue = adresseEntity.getRue();
+                    myClient.adresse.ville = adresseEntity.getVille();
 
                     clients.add(myClient);
                 }
@@ -133,7 +119,7 @@ public class RessourceClient {
         JSONObject clientJSON = null;
         Transaction tx = null;
 
-        try{
+        try {
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(jsonStr);
             token = (String) json.get("token");
@@ -142,11 +128,11 @@ public class RessourceClient {
             e.printStackTrace();
             return ReponseType.getNOTOK("Il manque des parametres (token, client)", false, null, null);
         }
-        if(!Token.tryToken(token))
+        if (!Token.tryToken(token))
             return Token.tokenNonValide();
 
         Client client = getClientFromJSON(clientJSON);
-        if(client == null)
+        if (client == null)
             return ReponseType.getNOTOK("Le JSON du client est mal forme veuillez verifier", false, null, null);
 
         ClientEntity clientEntity = new ClientEntity();
@@ -155,30 +141,32 @@ public class RessourceClient {
         clientEntity.setSiren(client.SIREN);
 
         int adresseId = client.adresse.getId();
-        if(adresseId == -1)
-            if(!client.adresse.addAdresse())
+        if (adresseId == -1)
+            if (!client.adresse.addAdresse())
                 return ReponseType.getNOTOK("Impossible de rajouter l'adresse sur la base", false, null, null);
         clientEntity.setAdresse(adresseId);
 
-        try(Session session = CreateSession.getSession()) {
+        try (Session session = CreateSession.getSession()) {
             tx = session.beginTransaction();
 
-            try{
+            try {
                 session.createQuery("FROM ClientEntity c WHERE c.siren = " + client.SIREN).getSingleResult();
                 return ReponseType.getNOTOK("Le SIREN " + client.SIREN + " existe deja veuillez le changer", true, tx, session);
-            } catch (NoResultException ignored) {}
+            } catch (NoResultException ignored) {
+            }
 
             try {
                 session.createQuery("FROM ClientEntity c WHERE c.nom = '" + client.nom + "'").getSingleResult();
                 return ReponseType.getNOTOK("Le client " + client.nom + " existe deja veuillez le changer", true, tx, session);
-            } catch (NoResultException ignored) {}
+            } catch (NoResultException ignored) {
+            }
 
             session.save(clientEntity);
             tx.commit();
             session.clear();
             session.close();
         } catch (HibernateException e) {
-            if(tx != null)
+            if (tx != null)
                 tx.rollback();
             e.printStackTrace();
             return ReponseType.getNOTOK("Impossible de sauvegarder le client sur la base", false, null, null);
@@ -189,25 +177,23 @@ public class RessourceClient {
     private Client getClientFromJSON(JSONObject json) {
         Client client = new Client();
 
-        try{
+        try {
             client.nom = (String) json.get("nom");
             client.SIREN = -1;
             client.SIREN = Integer.parseInt(((Long) json.get("SIREN")).toString());
 
             System.err.println("nom = " + client.nom + " siren = " + client.SIREN);
 
-            if(client.nom == null || client.SIREN == -1)
+            if (client.nom == null || client.SIREN == -1)
                 return null;
 
             //Ajout de l'adresse
             JSONObject adresse = (JSONObject) json.get("adresse");
-            if(adresse == null) {
+            if (adresse == null) {
                 System.err.println("L'adresse est mal formee");
                 return null;
-            }
-            else
-                if(!client.adresse.getFromJSON(adresse))
-                    return null;
+            } else if (!client.adresse.getFromJSON(adresse))
+                return null;
 
         } catch (NullPointerException e) {
             System.err.println("Erreur lors de la recuperation du client depuis un json parse impossible");
