249175db7eb5f771006518c412277fda74d25bcc
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-12 18:52:18 +0100

    Revert "Revert "Ajout de l'init sur le Login""
    This reverts commit 0e8d06242cc7c650694de74c7a955a665045c96c.

---
+++ GenieLogiciel/src/Modele/Staff/StaffInit.java
@@ -0,0 +1,13 @@
+package Modele.Staff;
+
+import java.util.ArrayList;
+
+public class StaffInit {
+    public ArrayList<String> competencesList, fonctionsList;
+    public Staff staff;
+
+    public StaffInit() {
+        competencesList = new ArrayList<>();
+        fonctionsList = new ArrayList<>();
+    }
+}
