777867a088e4d5ee63b2f8f033be1db232b9c92e
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-18 18:40:48 +0100

    ça marche

--- Python_Jules/projet.py
+++ Python_Jules/projet.py
@@ -8,10 +8,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas
-import fonctionConvertisseur as fctConv
-import fonctionRetabli as fctRet
-import fonctionFormatage as fctForm
-import fonctionFinalize as fctFin
 import nltk
 
 source = open("Exemple.csv","r")
@@ -24,20 +20,24 @@ temp = []
 
 for ligne in source:
 	if i == 1:
-		temp3 = [ligne.split()]
+		temp3 = ligne.split(";")
+		#print(temp3)
+		#print(len(temp3))
 		if len(temp3)==15:
-			temp += temp3
+			temp += [temp3]
 	i = 1
+
+#print(temp)
 
 resultat = ""
 
-for i in temp
+for i in temp:
 	resultat += "### Send POST to create Ticket\n"
 	resultat += "POST http://localhost:8080/genielog/ticket/create \n"
 	resultat += "Content-Type: text/plain\n"
 	resultat += "\n"
-	resultat += "{\"token\":\"96b29b22-cefb-4699-93b9-9fcc97aa003e\",\"ticket\":{\"adresse\":{\"codePostal\":\"69000\",\"numero\":32,\"rue\":\"RUE ALAINE\",\"ville\":\"LYON\"},\"categorie\":\"Etude\",\"competences\":[],\"demandeur\":{\"id\":12,\"nom\":\"RENARD\",\"prenom\":\"Maxime\"},\"objet\":\"TESTEST\",\"statut\":\"Resolu\",\"technicien\":{\"id\":2,\"nom\":\"VITTONE\",\"prenom\":\"Jules\"},\"type\":\"Demande\",\"priorite\":1}} \n"
-	resultat += "###"
+	resultat += "{\"token\":\"96b29b22-cefb-4699-93b9-9fcc97aa003e\",\"ticket\":{\"adresse\":{\"codePostal\":\"" + i[0] + "\",\"numero\":" + i[1] + ",\"rue\":\"" + i[2] + "\",\"ville\":\"" + i[3] + "\"},\"categorie\":\"" + i[4] + "\",\"competences\":[],\"demandeur\":{\"id\":" + i[9] + ",\"nom\":\"" + i[10] + "\",\"prenom\":\"" + i[11] + "\"},\"objet\":\"" + i[5] + "\",\"statut\":\"" + i[6] + "\",\"technicien\":{\"id\":" + i[12] + ",\"nom\":\"" + i[13] + "\",\"prenom\":\"" + i[14] + "\"},\"type\":\"" + i[7] + "\",\"priorite\":" + i[8] + "}} \n"
+	resultat += "###\n\n"
 
 numpy.savetxt(sortie,[resultat],fmt='%s')
 
