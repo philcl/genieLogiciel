39b90596ddbd56ab4a76c0cc32bb6cf5a6781ba4
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-29 23:49:19 +0100

    Update Map.java

--- GenieLogiciel/src/Modele/Stat/Map.java
+++ GenieLogiciel/src/Modele/Stat/Map.java
@@ -8,7 +8,11 @@ public class Map {
     public String label;
 
     public Map(String year,int size) {
-        data = new ArrayList<>(size);
+        data = new ArrayList<>();
+        for (int i=0; i<size;i++)
+        {
+            data.add(0L);
+        }
         label = year;
     }
 }
