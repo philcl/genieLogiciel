7ffb855051ea48ddc3d30b72889220ac91935051
jvittone <40358640+jvittone@users.noreply.github.com>
2020-03-03 10:30:59 +0100

    Maj importante des scripts python
    Les scripts générent tous les fichiers nécessaires

--- Python_Jules/projet.py
+++ Python_Jules/projet.py
@@ -1,20 +1,18 @@
 #Import des librairies
 
+print("Import")
+
 import numpy
-import sklearn
-import glob
-import os
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-import pandas
-import nltk
 
 source = open("Exemple.csv","r")
 
-sortie = "Exemple.http"
+sortie1 = "CreaDemandeur.http"
+sortie2 = "CreaStaff.http"
+sortie3 = "CreaTicket.http"
 
 i = 0
+
+print("Debut")
 
 temp = []
 
@@ -27,7 +25,35 @@ for ligne in source:
 			temp += [temp3]
 	i = 1
 
-#print(temp)
+print("Etape 1")
+
+resultat = ""
+
+for i in temp:
+	resultat += "### Send POST to change clientName to clientId\n"
+	resultat += "POST http://localhost:8080/genielog/demandeur/create \n"
+	resultat += "Content-Type: text/plain\n"
+	resultat += "\n"
+	resultat += "{\"token\":\"96b29b22-cefb-4699-93b9-9fcc97aa003e\", \"clientID\":21, \"demandeur\":{\"SIRET\":12345678912349, \"idAdresse\":12, \"demandeur\":{\"demandeurId\":" + i[9] + ",\"nom\":\"" + i[10] + "\",\"prenom\":\"" + i[11] + "\",\"sexe\":\"M\"}, \"telephone\":\"0645789878\"}} \n"
+	resultat += "###\n\n"
+
+numpy.savetxt(sortie1,[resultat],fmt='%s')
+
+print("Etape 2")
+
+resultat = ""
+
+for i in temp:
+	resultat += "### create User\n"
+	resultat += "POST http://localhost:8080/genielog/user/create \n"
+	resultat += "Content-Type: text/plain\n"
+	resultat += "\n"
+	resultat += "{\"token\":\"96b29b22-cefb-4699-93b9-9fcc97aa003e\",\"staff\":{\"staffId\":" + i[12] + ",\"staffUserName\":\"" + i[12] + "\",\"staffPassword\":\"1234\",\"staffMail\":\"test@test.fr\",\"staffAdress\":{\"numero\":12,\"rue\":\"avenue des champs\",\"ville\":\"PARIS\",\"codePostal\":\"75000\"},\"staffSurname\":\"" + i[13] + "\",\"staffName\":\"" + i[14] + "\",\"staffSexe\":\"M\",\"staffTel\":\"0504070105\",\"staffCompetency\":[\"\"],\"staffRole\":[\"Tech\"]}} \n"
+	resultat += "###\n\n"
+
+numpy.savetxt(sortie2,[resultat],fmt='%s')
+
+print("Etape 3")
 
 resultat = ""
 
@@ -39,6 +65,8 @@ for i in temp:
 	resultat += "{\"token\":\"96b29b22-cefb-4699-93b9-9fcc97aa003e\",\"ticket\":{\"adresse\":{\"codePostal\":\"" + i[0] + "\",\"numero\":" + i[1] + ",\"rue\":\"" + i[2] + "\",\"ville\":\"" + i[3] + "\"},\"categorie\":\"" + i[4] + "\",\"competences\":[],\"demandeur\":{\"id\":" + i[9] + ",\"nom\":\"" + i[10] + "\",\"prenom\":\"" + i[11] + "\"},\"objet\":\"" + i[5] + "\",\"statut\":\"" + i[6] + "\",\"technicien\":{\"id\":" + i[12] + ",\"nom\":\"" + i[13] + "\",\"prenom\":\"" + i[14] + "\"},\"type\":\"" + i[7] + "\",\"priorite\":" + i[8] + "}} \n"
 	resultat += "###\n\n"
 
-numpy.savetxt(sortie,[resultat],fmt='%s')
+numpy.savetxt(sortie3,[resultat],fmt='%s')
 
 source.close()
+
+print("Fin")
